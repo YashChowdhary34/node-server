275ef70e6ad89ca71be561c062a41f7493062372
yashchowdhary34 <yashchowdhary34@gmail.com>
2024-12-14 18:32:15 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+
+const requestListner = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  if (request.url == "/") {
+    response.write(`<html>`);
+    response.write(`<h1>Welcome to the Home Page</h1>`);
+    response.write(`<form action="/submit-details" method="POST">`);
+    response.write(
+      `<input type="text" id="name" name="username" placeholder="Enter your name"><br><br>`
+    );
+    response.write(`<label for="gender"> Male: </label>`);
+    response.write(`<input type="radio" id="male" name="gender" value="male">`);
+    response.write(`<label for="gender"> Female: </label>`);
+    response.write(
+      `<input type="radio" id="female" name="gender" value="female">`
+    );
+    response.write(`<button type="submit" value="Submit">Submit</button>`);
+    response.write(`</form>`);
+    response.write(`</html>`);
+    return response.end();
+  } else if (
+    request.url.toLowerCase() === "/submit-details" &&
+    request.method == "POST"
+  ) {
+    const chuckArray = [];
+    request.on("data", (chunk) => {
+      chuckArray.push(chunk);
+    });
+    request.on("end", () => {
+      const parsedBody = Buffer.concat(chuckArray).toString();
+      const params = new URLSearchParams(parsedBody);
+      const dataObj = Object.fromEntries(params);
+      const dataJson = JSON.stringify(dataObj);
+      fs.writeFileSync("data.txt", dataJson);
+    });
+
+    response.statusCode = 302;
+    response.setHeader("Location", "/");
+    return response.end();
+  }
+};
+
+const server = http.createServer(requestListner); // callback function inside createServer method
+
+server.listen(3000, () => {
+  console.log(`Server running on port: 3000`);
+});
